eb682e192cb1b229af659fa37e94c45f5875064f
Argenis De La Rosa <oftherose91@gmail.com>
2022-02-13 22:33:41 -0500

    Initial Commit

---
+++ bitcalc/bitcalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitcalcApp.swift
+//  bitcalc
+//
+//  Created by Argenis De La Rosa on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct bitcalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
